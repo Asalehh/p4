1b978513c0b7f9e1253b6155131b63db1896712f
Asalehh <ahmed@nilehoster.com>
2014-12-14 02:59:58 +0200

    Cleaning routes.php from debug route and other stuff

--- app/routes.php
+++ app/routes.php
@@ -12,73 +12,6 @@
 */
 
 
-Route::get('/db', function(){
-
-    $test = DB::select('SHOW DATABASES;');
-    if ($test){
-        print_r($test);
-    }
-
-});
-
-
-Route::get('/mail',function(){
-
-        Mail::send('emails.welcome', array('key' => 'value'), function($message)
-        {
-            $message->to('webmaster@nilehoster.com', 'John Smith')->subject('Welcome!');
-        });
-
-            return 'SENT';
-});
-
-
-Route::get('/debug', function() {
-
-    echo '<pre>';
-
-    echo '<h1>environment.php</h1>';
-    $path   = base_path().'/environment.php';
-
-    try {
-        $contents = 'Contents: '.File::getRequire($path);
-        $exists = 'Yes';
-    }
-    catch (Exception $e) {
-        $exists = 'No. Defaulting to `production`';
-        $contents = '';
-    }
-
-    echo "Checking for: ".$path.'<br>';
-    echo 'Exists: '.$exists.'<br>';
-    echo $contents;
-    echo '<br>';
-
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
-
-    echo '<h1>Debugging?</h1>';
-    if(Config::get('app.debug')) echo "Yes"; else echo "No";
-
-    echo '<h1>Database Config</h1>';
-    print_r(Config::get('database.connections.mysql'));
-
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    } 
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
-
-    echo '</pre>';
-
-});
-
-
 
 Route::get('/home', ['before'=>'guest','uses'=>'HomeController@index']);
 
@@ -125,55 +58,6 @@ Route::group(['prefix'=>'list'],function(){
     Route::get('/remove/{id?}', array('before'=>'auth','uses'=>'ListController@remove'));
 
 });
-
-
-/*
-
-Route::get('/debug', function() {
-
-    echo '<pre>';
-
-    echo '<h1>environment.php</h1>';
-    $path   = base_path().'/environment.php';
-
-    try {
-        $contents = 'Contents: '.File::getRequire($path);
-        $exists = 'Yes';
-    }
-    catch (Exception $e) {
-        $exists = 'No. Defaulting to `production`';
-        $contents = '';
-    }
-
-    echo "Checking for: ".$path.'<br>';
-    echo 'Exists: '.$exists.'<br>';
-    echo $contents;
-    echo '<br>';
-
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
-
-    echo '<h1>Debugging?</h1>';
-    if(Config::get('app.debug')) echo "Yes"; else echo "No";
-
-    echo '<h1>Database Config</h1>';
-    print_r(Config::get('database.connections.mysql'));
-
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    } 
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
-
-    echo '</pre>';
-
-});
-*/
 
 
 
